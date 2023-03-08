70cf6b5865d257607f3b6e14a1383430e1a25700
daudi13 <davouma13@gmail.com>
2023-03-08 07:46:28 +0300

    Update changes

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>This is the About page</div>
+}
+
+export default About;
---
+++ pages/profile/index.js
@@ -0,0 +1,5 @@
+const ProfileDefault = () => {
+  return <div>Profile default</div>
+}
+
+export default ProfileDefault;
