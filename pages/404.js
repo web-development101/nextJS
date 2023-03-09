b6a593d1d29a082d603eef03bc7ec4c20521d46f
daudi13 <davouma13@gmail.com>
2023-03-09 08:19:57 +0300

    Add 404 error page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import style from '@/styles/Home.module.css'
+
+const fourOFour = () => {
+  return (
+    <main className={style.main}>
+        <h1>Page not found</h1>
+    </main>
+  )
+}
+
+export default fourOFour
