1cdbfcd2baa1d1e10b79bf5e6ce52e183cc52d1c
daudi13 <davouma13@gmail.com>
2023-03-10 09:33:37 +0300

    Add update

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,12 @@
 import style from '@/styles/Home.module.css'
+import pageNotFound  from'../asset/404.png'
+import Link from 'next/link'
 
 const fourOFour = () => {
   return (
     <main className={style.main}>
-        <h1>Page not found</h1>
+      <h3>Page not found</h3>
+      <Link href="/">Click here to go back home</Link>
     </main>
   )
 }
---
+++ pages/coins/index.js
@@ -0,0 +1,8 @@
+const CoinList = () => {
+  return (<div></div>)
+}
+
+export const getStaticProps = async () => {
+  
+}
+export default CoinList
