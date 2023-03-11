c0f8f2f129a6b71b8adec04393e5df5770d7a81a
daudi13 <davouma13@gmail.com>
2023-03-11 21:52:41 +0300

    Add updates

--- pages/coins/index.js
+++ pages/coins/index.js
@@ -1,8 +1,26 @@
-const CoinList = () => {
-  return (<div></div>)
+import Axios from "axios"
+import style from '@/styles/Home.module.css'
+
+const CoinList = ({ coinData }) => {
+  console.log(coinData)
+  return (
+    <div className={style.main}>{coinData.coins.map((coin) => {
+      return (
+        <div>
+          <h1>{coin.name}</h1>
+          <img src={coin.icon} />
+          <p>{coin.price}</p>
+        </div>)
+  })}</div>)
 }
 
 export const getStaticProps = async () => {
-  
+  const data = await Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
+
+  return {
+    props: {
+      coinData: data.data,
+    }
+  }
 }
 export default CoinList
